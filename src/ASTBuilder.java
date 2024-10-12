617d76eb12b19900d823f373b7c68679f1a41d3c
ammarqd <ammar-g@hotmail.co.uk>
2024-10-12 14:58:03 +0100

    Add class abstract syntax tree implementation to ASTBuilder

--- src/ASTBuilder.java
+++ src/ASTBuilder.java
@@ -8,10 +8,27 @@ public class ASTBuilder extends CoolParserBaseVisitor<Tree> {
     public Tree visitProgram(CoolParser.ProgramContext ctx) {
 
         ProgramNode p = new ProgramNode(ctx.getStart().getLine());
-        for (CoolParser.CoolClassContext c:ctx.coolClass()) {
+        for (CoolParser.CoolClassContext c : ctx.coolClass()) {
             p.add((ClassNode)visitCoolClass(c));
         }
         return p;
+    }
+
+    @Override
+    public Tree visitCoolClass(CoolParser.CoolClassContext ctx) {
+
+        Symbol className = new Symbol(ctx.TYPEID(0).getText(), ctx.getStart().getLine());
+        Symbol parentName = new Symbol(
+                ctx.INHERITS() != null ? ctx.TYPEID(1).getText() : "Object",
+                ctx.getStart().getLine()
+        );
+        Symbol fileName = new Symbol(ctx.start.getInputStream().getSourceName(), ctx.getStart().getLine());
+
+        ClassNode classNode = new ClassNode(1, className, parentName, fileName);
+        for (CoolParser.FeatureContext f : ctx.feature()) {
+            classNode.add((FeatureNode)visitFeature(f));
+        }
+        return classNode;
     }
 
 }
