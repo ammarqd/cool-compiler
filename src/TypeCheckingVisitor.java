f8f2eaac62104f4181147289caf1ce24e97cb251
ammarqd <ammarqd@outlook.com>
2024-11-07 18:55:14 +0000

    Set Int constant types for AST in TypeCheckingVisitor

--- src/TypeCheckingVisitor.java
+++ src/TypeCheckingVisitor.java
@@ -1,5 +1,14 @@
+import ast.*;
 import ast.visitor.BaseVisitor;
 
-public class TypeCheckingVisitor extends BaseVisitor<Object, Object> {
+class MyContext { }
+
+public class TypeCheckingVisitor extends BaseVisitor<Object, MyContext> {
+
+    @Override
+    public Symbol visit(IntConstNode node, MyContext data) {
+        node.setType(TreeConstants.Int);
+        return TreeConstants.Int;
+    }
 
 }
