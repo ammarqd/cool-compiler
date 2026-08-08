aa563ad7331a21bfeb32e70a20777858237cc8c2
ammarqd <ammarqd@outlook.com>
2026-08-08 12:53:32 +0100

    Implement attribute read and write access in AttrInfo

--- src/Cgen.java
+++ src/Cgen.java
@@ -113,13 +113,13 @@ public class Cgen  {
 
         @Override
         public String emitRef(String optionalDest) {
-            /* TODO */
-            return null;
+            emitter.emitLoad(optionalDest, CgenConstants.DEFAULT_OBJFIELDS + offset, CgenConstants.SELF);
+            return optionalDest;
         }
 
         @Override
         public void emitUpdate(String source) {
-            /* TODO */
+            emitter.emitStore(source, CgenConstants.DEFAULT_OBJFIELDS + offset, CgenConstants.SELF);
         }
     }
 
